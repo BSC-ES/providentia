cc5c3e0456e745a53c31ebf071229097c1343d24
Dene Bowdalo <dene.bowdalo@bsc.es>
2019-07-15 17:58:47 +0200

    Added interpolation directory

--- interpolation/configuration.py
+++ interpolation/configuration.py
@@ -16,7 +16,7 @@ experiments_to_process = ['a1vv']
 
 #define complete list of desired species to process (this is later filtered based on availability of species in experiment directories)
 species_to_process = ['sconcco','sconchno3','sconcnh3','sconcisop','sconcno','sconcno2','sconco3','sconcpan','sconcso2',
-                      'pm10','pm2p5','p1',
+                      'pm10','pm2p5','pm1',
                       'sconcal','sconcas','sconcbc','sconcc','sconcca','sconccd','sconccl','sconccobalt','sconccr','sconccu','sconcec','sconcfe','sconchg','sconck','sconcmg','sconcmn','sconcna','sconcnh4','sconcni','sconcno3','sconcoc','sconcpb','sconcse','sconcso4','sconcv','sconczn',
                       'pm10al','pm10as','pm10bc','pm10c','pm10ca','pm10cd','pm10cl','pm10cobalt','pm10cr','pm10cu','pm10ec','pm10fe','pm10hg','pm10k','pm10mg','pm10mn','pm10na','pm10nh4','pm10ni','pm10no3','pm10oc','pm10pb','pm10se','pm10so4','pm10v','pm10zn',
                       'pm2p5al','pm2p5as','pm2p5bc','pm2p5c','pm2p5ca','pm2p5cd','pm2p5cl','pm2p5cobalt','pm2p5cr','pm2p5cu','pm2p5ec','pm2p5fe','pm2p5hg','pm2p5k','pm2p5mg','pm2p5mn','pm2p5na','pm2p5nh4','pm2p5ni','pm2p5no3','pm2p5oc','pm2p5pb','pm2p5se','pm2p5so4','pm2p5v','pm2p5zn',
