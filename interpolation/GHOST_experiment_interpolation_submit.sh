9610445b269ebf11cd0f43eaceffb66ded728a08
Dene Bowdalo <denebowdalo@googlemail.com>
2019-07-16 17:13:35 +0200

    Updated Interpolation Directory

--- interpolation/GHOST_experiment_interpolation_submit.sh
+++ interpolation/GHOST_experiment_interpolation_submit.sh
@@ -6,4 +6,9 @@
 #SBATCH --mem-per-cpu=4G
 #SBATCH --time=48:00:00                                            
 
+module load Python/2.7.15-foss-2018b
+module load Cartopy/0.16.0-foss-2018b-Python-2.7.15
+module load geopy/1.18.1-foss-2018b-Python-2.7.15
+module load numpy/1.16.0-foss-2018b-Python-2.7.15
+module load Shapely/1.6.4.post2-foss-2018b-Python-2.7.15
 srun --cpu-bind=core --output=management_logs/$SLURM_JOB_ID.out --error=management_logs/$SLURM_JOB_ID.err python -u GHOST_experiment_interpolation_submission.py $SLURM_JOB_ID $PWD
