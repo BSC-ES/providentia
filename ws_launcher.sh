32d18d98e061d9c7e52a2e8fb6050788032ef656
Dene Bowdalo <dene.bowdalo@bsc.es>
2019-07-24 11:29:13 +0200

    Fixed interactive tool matrix bug

--- ws_launcher.sh
+++ ws_launcher.sh
@@ -4,6 +4,8 @@ module load matplotlib/3.1.1-foss-2015a-Python-3.7.3
 module load netcdf4-python/1.5.1.2-foss-2015a-Python-3.7.3
 module load numpy/1.16.4-foss-2015a-Python-3.7.3
 module load pandas/0.24.2-foss-2015a-Python-3.7.3
+module load PyQt5/5.13.0-foss-2015a-Python-3.7.3
 module load seaborn/0.9.0-foss-2015a-Python-3.7.3
 module load scipy/1.3.0-foss-2015a-Python-3.7.3
+module load Qt/5.13.0-test-foss-2015a
 python GHOST_interactive.py 
