cd84df9896d410c556a87cc46b05ac7b7fcf8f62
Dene Bowdalo <dene.bowdalo@bsc.es>
2019-07-24 16:25:59 +0200

    updated ws launcher

--- ws_launcher.sh
+++ ws_launcher.sh
@@ -1,6 +1,5 @@
 module purge
 module load Python/3.7.3-foss-2015a
-module load Qt/5.13.0-foss-2015a
 module load Cartopy/0.17.0-foss-2015a-Python-3.7.3
 module load matplotlib/3.1.1-foss-2015a-Python-3.7.3
 module load netcdf4-python/1.5.1.2-foss-2015a-Python-3.7.3
@@ -9,4 +8,5 @@ module load pandas/0.24.2-foss-2015a-Python-3.7.3
 module load PyQt5/5.13.0-foss-2015a-Python-3.7.3
 module load seaborn/0.9.0-foss-2015a-Python-3.7.3
 module load scipy/1.3.0-foss-2015a-Python-3.7.3
+module load Qt/5.13.0-foss-2015a
 python GHOST_interactive.py 
