bbc2fc2822b9c946322c358b7c506e013d4da47a
Dene Bowdalo <denebowdalo@googlemail.com>
2019-07-29 13:09:48 +0200

    Fixed bug associated with experiment directory reading

--- ws_launcher.sh
+++ ws_launcher.sh
@@ -8,5 +8,5 @@ module load pandas/0.24.2-foss-2015a-Python-3.7.3
 module load PyQt5/5.13.0-foss-2015a-Python-3.7.3
 module load seaborn/0.9.0-foss-2015a-Python-3.7.3
 module load scipy/1.3.0-foss-2015a-Python-3.7.3
-module load Qt/5.13.0-foss-2015a
+#module load Qt/5.13.0-foss-2015a
 python GHOST_interactive.py 
