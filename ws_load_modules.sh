24bdf6b48747736c93ce5b8f3b1ec5a7bf77129c
Dene Bowdalo <dene.bowdalo@bsc.es>
2019-08-02 12:49:09 +0200

    Adaptation for running on workstations/power9, and various bug fixes/optimisations

---
+++ ws_load_modules.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+module purge
+#import modules
+module load Python/3.7.3-foss-2015a
+module load Cartopy/0.17.0-foss-2015a-Python-3.7.3
+module load matplotlib/3.1.1-foss-2015a-Python-3.7.3
+module load netcdf4-python/1.5.1.2-foss-2015a-Python-3.7.3
+module load numpy/1.16.4-foss-2015a-Python-3.7.3
+module load pandas/0.24.2-foss-2015a-Python-3.7.3
+module load PyQt5/5.13.0-foss-2015a-Python-3.7.3
+module load seaborn/0.9.0-foss-2015a-Python-3.7.3
+module load scipy/1.3.0-foss-2015a-Python-3.7.3
