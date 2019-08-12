0071b030c5d1b600073f2e3638ae842cfa8651ab
Dene Bowdalo <denebowdalo@googlemail.com>
2019-08-12 16:59:37 +0200

    Updated all repository name references from GHOST_interactive to Providentia

--- p9_load_modules.sh
+++ p9_load_modules.sh
@@ -12,5 +12,5 @@ module load PyQt5/5.13.0-foss-2018b-Python-3.7.0
 module load scipy/1.3.0-foss-2018b-Python-3.7.0 
 
 #get allocation of resources on CTE-POWER machine
-#DEFAULT: 3 hours walltime, 16 CPUs and 10Gb per CPU
-salloc -t 03:00:00 -n 1 -c 16 --mem-per-cpu=10Gb -J GI --x11
+#DEFAULT: 3 hours walltime, 16 CPUs and total memory of 50Gb
+salloc -t 03:00:00 -n 1 -c 16 --mem=50Gb -J GI --x11
