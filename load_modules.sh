77bf2d93bd2b98779c9592d326530a3b30a74421
Dene Bowdalo <dene.bowdalo@bsc.es>
2019-09-26 15:32:34 +0200

    Fixed resource allocation bug

--- load_modules.sh
+++ load_modules.sh
@@ -14,14 +14,12 @@ if [[ $machine == *"power.cte"* ]]; then
     module load PyQt5/5.13.0-foss-2018b-Python-3.7.0
     module load scipy/1.3.0-foss-2018b-Python-3.7.0
     #get allocation of resources on CTE-POWER machine
-    #DEFAULT: 3 hours walltime, 16 CPUs and total memory of 50Gb
-    salloc -t 03:00:00 -n 1 -c 16 --mem=50Gb -J PRV --x11
+    salloc -t 02:00:00 -n 1 -c 16 --mem=50Gb -J PRV -q debug --x11=first
 #Marenustrum4
 elif [[ $machine == *"bsc.mn"* ]]; then
     module load gcc/5.4.0 intel/2018.4 impi/2018.4 mkl/2018.4 python/3.7.4_ES udunits/2.2.25  hdf5/1.10.5  netcdf/4.4.1.1_lf geos/3.6.1 proj/4.9.3 gdal/2.2.3
     #get allocation of resources on MN4 machine
-    #DEFAULT: 3 hours walltime, 16 CPUs and total memory of 50Gb
-    salloc -t 03:00:00 -n 1 -c 16 --mem=50Gb -J PRV --x11
+    salloc -t 02:00:00 -n 1 -c 16 -J PRV -q debug --x11=first
 #Workstations
 else
     module load Python/3.7.3-foss-2015a
