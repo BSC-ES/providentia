ae5e7ffaceaed14a3add93048ac882b3d1dbef7c
Dene Bowdalo <denebowdalo@googlemail.com>
2019-10-01 15:48:23 +0200

    Updated modules to load

--- load_modules.sh
+++ load_modules.sh
@@ -16,7 +16,7 @@ if [ $BSC_MACHINE == "power" ]; then
     salloc -t 02:00:00 -n 1 -c 16 --mem=50Gb -J PRV -q debug --x11=first
 #Marenustrum4
 elif [ $BSC_MACHINE == "mn4" ]; then
-    module load gcc/5.4.0 intel/2018.4 impi/2018.4 mkl/2018.4 python/3.7.4_ES udunits/2.2.25  hdf5/1.10.5  netcdf/4.4.1.1_lf geos/3.6.1 proj/4.9.3 gdal/2.2.3
+    module load bsc/1.0 gcc/5.4.0 intel/2017.4 impi/2017.4 mkl/2017.4 python/3.7.4_ES udunits/2.2.25  hdf5/1.10.5  netcdf/4.4.1.1_lf geos/3.6.1 proj/4.9.3 gdal/2.2.3
     #get allocation of resources on MN4 machine
     salloc -t 02:00:00 -n 1 -c 16 -J PRV -q debug --x11=first
 #Workstations
