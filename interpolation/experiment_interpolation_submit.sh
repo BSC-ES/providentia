e4bed80b10e5a183a96914c206a4b6c0bfd629fd
Dene Bowdalo <dene.bowdalo@bsc.es>
2019-10-17 06:28:09 +0200

    updated interpolation code

--- interpolation/experiment_interpolation_submit.sh
+++ interpolation/experiment_interpolation_submit.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-#SBATCH --job-name=PRVI_S                                         
+#SBATCH --job-name=PRVI                                         
 #SBATCH --ntasks=1                                                
-#SBATCH −−cpus−per−task=1
-#SBATCH --time=48:00:00                                            
+#SBATCH --time=01:00:00                                            
 #SBATCH --qos=debug
 #SBATCH --output=/dev/null
 #SBATCH --error=/dev/null
@@ -11,4 +10,4 @@
 #load modules
 source load_modules.sh
 
-srun --cpu-bind=core --output=management_logs/$SLURM_JOB_ID.out --error=management_logs/$SLURM_JOB_ID.err python -u experiment_interpolation_submission.py $SLURM_JOB_ID
+srun --cpu-bind=core --output=management_logs/$SLURM_JOB_ID.out python -u experiment_interpolation_submission.py $SLURM_JOB_ID
