cdab467fa1e05e65ea29460b3e3bad179d6c9caf
Dene Bowdalo <denebowdalo@googlemail.com>
2019-11-06 11:23:23 +0100

    updated interpolation code to use greasy

--- interpolation/defined_experiments.py
+++ interpolation/defined_experiments.py
@@ -23,16 +23,46 @@ defined_experiments_dictionary = {#---------------------------------------------
                                    'a1yz':{'esarchive':'/esarchive/exp/monarch/a1yz/'},
                                    #a1zc - CO boundary conditions in volume mixing ratio
                                    'a1zc':{'esarchive':'/esarchive/exp/monarch/a1zc/'},		
-                                   #a28j
+                                   #other
+                                   'a1wdwodu':{'esarchive':'/esarchive/exp/monarch/a1wdwodu/'},
+                                   'a21m':{'esarchive':'/esarchive/exp/monarch/a21m/'},
+                                   'a22r':{'esarchive':'/esarchive/exp/monarch/a22r/'},
+                                   'a22s':{'esarchive':'/esarchive/exp/monarch/a22s/'},
                                    'a28j':{'esarchive':'/esarchive/exp/monarch/a28j/'},
-                                   #a22r	
+                                   'a28j_v2':{'esarchive':'/esarchive/exp/monarch/a28j_v2/'},
+                                   'a28jwodu':{'esarchive':'/esarchive/exp/monarch/a28jwodu/'},
+                                   'a29d':{'esarchive':'/esarchive/exp/monarch/a29d/'},
+                                   'a29d_v2':{'esarchive':'/esarchive/exp/monarch/a29d_v2/'},
+                                   'a29dwodu':{'esarchive':'/esarchive/exp/monarch/a29dwodu/'},
+                                   'a2ba':{'esarchive':'/esarchive/exp/monarch/a2ba/'},
+                                   'a2bg':{'esarchive':'/esarchive/exp/monarch/a2bg/'},
+                                   'a2bk':{'esarchive':'/esarchive/exp/monarch/a2bk/'},
+                                   'a2bz':{'esarchive':'/esarchive/exp/monarch/a2bz/'},
                                    'a22r':{'esarchive':'/esarchive/exp/monarch/a22r/'},
                                    #----------------------------------------------------------------------------#
                                    #CALIOPE OPERATIONAL experiments
                                    'b012':{'esarchive':'/esarchive/exp/wrf-hermes-cmaq/b012/'},
                                    #----------------------------------------------------------------------------#                         
                                    #AUTO-WRF-HERMES-CMAQ experiments
+                                   'a1ay':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a1ay/'}, 
+                                   'a1uo':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a1uo/'},
                                    'a148':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a148/'},
+                                   'a1wz':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a1wz/'},
+                                   'a1y6':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a1y6/'},
+                                   'a1y7':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a1y7/'},
+                                   'a1y8':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a1y8/'},
+                                   'a1y9':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a1y9/'},
+                                   'a1ya':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a1ya/'},
+                                   'a1zk':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a1zk/'},
+                                   'a1zm':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a1zm/'},
+                                   'a26h':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a26h/'},
+                                   'a26j':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a26j/'},
+                                   'a26o':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a26o/'},
+                                   'a272':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a272/'},  
+                                   'a29h':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a29h/'},
+                                   'a29i':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a29i/'},
+                                   'a2a1':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a2a1/'},
+                                   'a2a6':{'esarchive':'/esarchive/exp/auto-wrf-hermes-cmaq/a2a6/'},
                                    #----------------------------------------------------------------------------#
                                    #CAMS 50 experiments
                                    #CAMS 50 ensemble median
--- interpolation/load_modules.sh
+++ interpolation/load_modules.sh
@@ -3,6 +3,8 @@ module purge
 
 #CTE POWER
 if [ $BSC_MACHINE == "power" ]; then
+    module loaded gcc/6.4.0
+    module loaded openmpi/3.0.0
     module load Python/3.7.0-foss-2018b
     module load Cartopy/0.17.0-foss-2018b-Python-3.7.0
     module load cftime/1.0.3.4-foss-2018b-Python-3.7.0
@@ -11,7 +13,9 @@ if [ $BSC_MACHINE == "power" ]; then
     module load pyproj/2.2.1-foss-2018b-Python-3.7.0
     module load scipy/1.3.0-foss-2018b-Python-3.7.0
     module load Shapely/1.6.4.post2-foss-2018b-Python-3.7.0
+    module load greasy/latest
+    module load NCO/4.7.2-foss-2018b
 #Marenustrum4
 elif [ $BSC_MACHINE == "mn4" ]; then
-    module load bsc/1.0 gcc/5.4.0 intel/2017.4 impi/2017.4 mkl/2017.4 python/3.7.4_ES udunits/2.2.25 hdf5/1.10.5 netcdf/4.4.1.1_lf geos/3.6.1 proj/4.9.3 gdal/2.2.3
+    module load bsc/1.0 gcc/5.4.0 intel/2017.4 impi/2017.4 mkl/2017.4 python/3.7.4_ES udunits/2.2.25 hdf5/1.10.5 netcdf/4.4.1.1 geos/3.6.1 proj/4.9.3 gdal/2.2.3 gsl/2.4 greasy/latest nco/4.6.7
 fi
