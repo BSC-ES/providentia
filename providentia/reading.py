92e1fc869c0e0ad6d8b92f27d2aaf600b12571b7
Amalia Vradi <amalia.vradi@bsc.es>
2019-12-13 17:38:21 +0100

    fix imports, separate one read function

--- providentia/reading.py
+++ providentia/reading.py
@@ -1,26 +1,45 @@
-import netCDF4
+from netCDF4 import Dataset
 import numpy as np
 import pandas as pd
 
 
-def read_netcdf_station_information(relevant_file, metadata_vars_to_read):
+def read_netcdf_station_information(tuple_to_read):
 
     """ Function that handles reading of observational
     desired station metadata in a netCDF file,
-    returning a dictionary with read metadata. """
+    returning a dictionary with read metadata.
+
+    Parameters
+    ----------
+    tuple_to_read: tuple of the form (file_to_read, [metadata_vars_to_read])
+
+    """
 
     # read netCDF frame
-    ncdf_root = netCDF4.Dataset(relevant_file)
+    ncdf_root = Dataset(tuple_to_read[0])
 
     # read all desired metadata, placing it within a dictionary by variable name
     read_metadata = {}
-    for meta_var in metadata_vars_to_read:
+    for meta_var in tuple_to_read[1]:
         read_metadata[meta_var] = ncdf_root[meta_var][:]
 
     # close netCDF
     ncdf_root.close()
 
     return read_metadata
+
+
+def drop_nans(data):
+
+    """function that returns numpy object of lists of station data with NaNs removed"""
+
+    # reshape numpy array to have lists of data per station
+    data = data.tolist()
+    # iterate through each list of station data and remove NaNs
+    for station_ii, station_data in enumerate(data):
+        data[station_ii] = np.array(station_data)[~np.isnan(station_data)]
+    # return numpy object of lists of station data with NaNs removed
+    return np.array(data)
 
 
 # def read_netcdf_data(relevant_file):
