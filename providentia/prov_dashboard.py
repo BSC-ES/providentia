b286bfb6b918ba3184df6278ad6beef4800bbb9c
Francesco <francesco.benincasa@bsc.es>
2020-01-22 15:24:06 +0100

    Fixed an issue on temp_agg_markersize parameter and on n_cpus parameterº

--- providentia/prov_dashboard.py
+++ providentia/prov_dashboard.py
@@ -1149,7 +1149,7 @@ class GenerateProvidentiaDashboard(QtWidgets.QWidget, ProvConfiguration):
         elif self.read_type == 'parallel':
 
             # setup pool of N workers on N CPUs
-            pool = multiprocessing.Pool(n_CPUs)
+            pool = multiprocessing.Pool(n_cpuS)
 
             # read netCDF files in parallel
             tuples_list = [(file_name, metadata_vars_to_read) for file_name in relevant_files]
@@ -1275,7 +1275,7 @@ class GenerateProvidentiaDashboard(QtWidgets.QWidget, ProvConfiguration):
         elif self.read_type == 'parallel':
 
             # setup pool of N workers on N CPUs
-            pool = multiprocessing.Pool(n_CPUs)
+            pool = multiprocessing.Pool(self.n_cpus)
 
             # read netCDF files in parallel
             tuple_arguments = [(file_name, self.time_array, self.station_references, self.active_species,
