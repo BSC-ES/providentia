047db0a4b8cab7afbf3d90607c7925699683f102
Dene Bowdalo <dene.bowdalo@bsc.es>
2020-02-19 20:30:45 +0100

    Updated interpolation, fixed issue not removing fill values, fixed period filter bug

--- providentia/prov_dashboard.py
+++ providentia/prov_dashboard.py
@@ -673,30 +673,36 @@ class ProvidentiaMainWindow(QtWidgets.QWidget, ProvConfiguration):
             # iterate through all available grids
             for grid in available_grids:
 
-                # test first if interpolated directory exists before trying to get files from it
-                # if it does not exit, continue
-                if not os.path.exists(
-                        '%s/%s/%s/%s/%s/%s/%s' % (self.exp_root, self.ghost_version, experiment, grid,
-                                                  self.selected_resolution, self.selected_species,
-                                                  self.selected_network)):
-                    continue
-                else:
-                    # get all experiment netCDF files by experiment/grid/selected
-                    # resolution/selected species/selected network
-                    network_files = os.listdir(
-                        '%s/%s/%s/%s/%s/%s/%s' % (self.exp_root, self.ghost_version, experiment, grid, self.selected_resolution,
-                                                  self.selected_species, self.selected_network))
-                    # get start YYYYMM yearmonths of data files
-                    network_files_yearmonths = [int(f.split('_')[-1][:6]+'01') for f in network_files]
-                    # limit data files to just those within date range
-                    valid_network_files_yearmonths = \
-                        [ym for ym in network_files_yearmonths if (ym >= self.selected_start_date_firstdayofmonth) &
-                         (ym < self.selected_end_date)]
+                #get all available ensemble member numbers
+                available_member_numbers = os.listdir('%s/%s/%s/%s' % (self.exp_root, self.ghost_version, experiment, grid))
 
-                    # if have some valid data files for experiment-grid, add experiment grid
-                    # (with associated yearmonths) to dictionary
-                    if len(valid_network_files_yearmonths) > 0:
-                        self.available_experiment_data['%s-%s' % (experiment, grid)] = valid_network_files_yearmonths
+                #iterate through all available ensemble member numbers
+                for member in available_member_numbers:
+
+                    # test first if interpolated directory exists before trying to get files from it
+                    # if it does not exit, continue
+                    if not os.path.exists(
+                            '%s/%s/%s/%s/%s/%s/%s/%s' % (self.exp_root, self.ghost_version, experiment, grid, member,
+                                                      self.selected_resolution, self.selected_species,
+                                                      self.selected_network)):
+                        continue
+                    else:
+                        # get all experiment netCDF files by experiment/grid/selected
+                        # resolution/selected species/selected network
+                        network_files = os.listdir(
+                            '%s/%s/%s/%s/%s/%s/%s/%s' % (self.exp_root, self.ghost_version, experiment, grid, member,
+                                                      self.selected_resolution, self.selected_species, self.selected_network))
+                        # get start YYYYMM yearmonths of data files
+                        network_files_yearmonths = [int(f.split('_')[-1][:6]+'01') for f in network_files]
+                        # limit data files to just those within date range
+                        valid_network_files_yearmonths = \
+                            [ym for ym in network_files_yearmonths if (ym >= self.selected_start_date_firstdayofmonth) &
+                             (ym < self.selected_end_date)]
+
+                        # if have some valid data files for experiment-grid-member, add experiment-grid-member key
+                        # (with associated yearmonths) to dictionary
+                        if len(valid_network_files_yearmonths) > 0:
+                            self.available_experiment_data['%s-%s-%s' % (experiment, grid, member)] = valid_network_files_yearmonths
 
         # get list of available experiment-grid names
         self.experiments_menu['checkboxes']['labels'] = np.array(
@@ -1194,10 +1200,11 @@ class ProvidentiaMainWindow(QtWidgets.QWidget, ProvConfiguration):
             experiment_grid_split = data_label.split('-')
             active_experiment = experiment_grid_split[0]
             active_grid = experiment_grid_split[1]
+            active_member = experiment_grid_split[2]
             file_root = \
-                '%s/%s/%s/%s/%s/%s/%s/%s_' % (self.exp_root, self.ghost_version, active_experiment,
-                                              active_grid, self.active_resolution, self.active_species,
-                                              self.active_network, self.active_species)
+                '%s/%s/%s/%s/%s/%s/%s/%s/%s_' % (self.exp_root, self.ghost_version, active_experiment,
+                                              active_grid, active_member, self.active_resolution, 
+                                              self.active_species, self.active_network, self.active_species)
             relevant_file_start_dates = sorted(self.available_experiment_data[data_label])
 
         # create list of relevant files to read
