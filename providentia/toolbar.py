26066a3bf122fd0d25ea3b96a8e2c3ba0c0b9894
Amalia <amalia.vradi@bsc.es>
2020-09-15 18:51:41 +0200

    move writting functions to new module

--- providentia/toolbar.py
+++ providentia/toolbar.py
@@ -1,10 +1,11 @@
+""" Navigation toolbar and buttons/options functions"""
 import os
 
 from PyQt5 import QtWidgets
 import matplotlib
 from matplotlib.backends import qt_compat
 from matplotlib.backends.backend_qt5agg import NavigationToolbar2QT
-from .prov_canvas import MPLCanvas
+from .writting import write_data_npz
 
 
 class NavigationToolbar(NavigationToolbar2QT):
@@ -23,26 +24,27 @@ class NavigationToolbar(NavigationToolbar2QT):
         (None, None, None, None)
     )
 
-    def save_data_button(self):
-        filetypes = {'Numpy file': ['npz']}
-        sorted_filetypes = sorted(filetypes.items())
-        startpath = os.path.expanduser(matplotlib.rcParams['savefig.directory'])
-        start = os.path.join(startpath, 'default_filename')
-        filters = []
-        selectedFilter = None
-        for name, exts in sorted_filetypes:
-            exts_list = " ".join(['*.%s' % ext for ext in exts])
-            filter = '%s (%s)' % (name, exts_list)
-            filters.append(filter)
-            filters = ';;'.join(filters)
-            fname, filter = qt_compat._getSaveFileName(self.parent(), "Choose a filename to save to",
-                                                       start, filters, selectedFilter)
-            if fname:
-                # Save dir for next time, unless empty str (i.e., use cwd).
-                if startpath != "":
-                    matplotlib.rcParams['savefig.directory'] = (os.path.dirname(fname))
-                    try:
-                        MPLCanvas.write_data_npz(self, fname)
-                    except Exception as e:
-                        QtWidgets.QMessageBox.critical(self, "Error saving file", str(e), QtWidgets.QMessageBox.Ok,
-                                                       QtWidgets.QMessageBox.NoButton)
+
+def save_data(mpl_canvas):
+    filetypes = {'Numpy file': ['npz']}
+    sorted_filetypes = sorted(filetypes.items())
+    startpath = os.path.expanduser(matplotlib.rcParams['savefig.directory'])
+    start = os.path.join(startpath, 'default_filename')
+    filters = []
+    selectedFilter = None
+    for name, exts in sorted_filetypes:
+        exts_list = " ".join(['*.%s' % ext for ext in exts])
+        filter = '%s (%s)' % (name, exts_list)
+        filters.append(filter)
+        filters = ';;'.join(filters)
+        fname, filter = qt_compat._getSaveFileName(None, "Choose a filename to save to",
+                                                   start, filters, selectedFilter)
+        if fname:
+            # Save dir for next time, unless empty str (i.e., use cwd).
+            if startpath != "":
+                matplotlib.rcParams['savefig.directory'] = (os.path.dirname(fname))
+                try:
+                    write_data_npz(mpl_canvas, fname)
+                except Exception as e:
+                    QtWidgets.QMessageBox.critical(mpl_canvas, "Error saving file", str(e), QtWidgets.QMessageBox.Ok,
+                                                   QtWidgets.QMessageBox.NoButton)
