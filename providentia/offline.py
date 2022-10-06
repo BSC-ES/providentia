9dbc3a25ec6a835ed1f72dbddc620b7a7ac5f4cf
Alba Vilanova <alba.vilanova@outlook.com>
2022-10-06 09:34:51 +0200

    Add warning when kernel density cannot be calculated and change annotations format

--- providentia/offline.py
+++ providentia/offline.py
@@ -272,8 +272,8 @@ class ProvidentiaOffline(ProvConfiguration, InitStandards):
                                 continue
 
                             # make plot
+                            print('Making summary {0}'.format(plot_type))
                             self.make_plot('summary', plot_type, plot_options, networkspeci)
-                            print('Summary plot type {0} has been created'.format(plot_type))
 
                         self.n_total_pages = len(self.plot_dictionary)
 
@@ -326,11 +326,11 @@ class ProvidentiaOffline(ProvConfiguration, InitStandards):
                                         continue
                                 
                                 # make plot
+                                print('Making station {2} for {3} ({0}/{1})'.format(i, 
+                                                                                              len(valid_station_inds),
+                                                                                              plot_type, 
+                                                                                              self.current_station_name))                                
                                 self.make_plot('station', plot_type, plot_options, networkspeci)
-                                print('Station {2} plot type has been created for {3} ({0}/{1})'.format(i, 
-                                                                                                        len(valid_station_inds),
-                                                                                                        plot_type, 
-                                                                                                        self.current_station_name))
 
             # do formatting to axes per networkspeci
             # create colourbars
