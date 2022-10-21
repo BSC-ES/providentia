53be2386c6603aba69a52e58c14faa4ecea64fa5
Alba Vilanova <alba.vilanova@bsc.es>
2022-10-21 10:29:05 +0200

    Fix tables

--- providentia/plot.py
+++ providentia/plot.py
@@ -1148,16 +1148,23 @@ class Plot:
         # round dataframe
         stats_df = stats_df.round(plot_characteristics['round_decimal_places'])
 
-        # set row_colours
-        if 'row_colours' in plot_characteristics:
-            if plot_characteristics['row_colours']:
-                for data_label in col_labels:
-                    if data_label == 'observations':
-                        plot_characteristics['plot']['rowColours'] = ['white']
-                    else:
-                        exp_label = list(self.read_instance.experiments.keys())[list(self.read_instance.experiments.values()).index(data_label)]
-                        plot_characteristics['plot']['rowColours'] = [self.read_instance.plotting_params[exp_label]['colour']]
-
+        # set row_colours and col_colours
+        if statsummary:
+            if 'row_colours' in plot_characteristics:
+                if plot_characteristics['row_colours']:
+                    plot_characteristics['plot']['rowColours'] = ['white' if data_label == 'observations' else 
+                                                                self.read_instance.plotting_params[data_label]['colour'] 
+                                                                for data_label in row_labels]
+        else:
+            if 'col_colours' in plot_characteristics:
+                if plot_characteristics['col_colours']:
+                    for data_label in col_labels:
+                        if data_label == 'observations':
+                            plot_characteristics['plot']['colColours'] = ['white']
+                        else:
+                            exp_label = list(self.read_instance.experiments.keys())[list(self.read_instance.experiments.values()).index(data_label)]
+                            plot_characteristics['plot']['colColours'] = [self.read_instance.plotting_params[exp_label]['colour']]
+    
         # if plot is statsummary, then remove bias from col_labels, and use alises for row_labels
         if statsummary:
             col_labels = [col_label.split('_bias')[0] if '_bias' in col_label else col_label for col_label in col_labels]
