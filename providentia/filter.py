cc9f6d6a8c8d409fcff8a0fec86e0a9651f938c9
Alba Vilanova Cortezon <alba.vilanova@bsc.es>
2023-03-16 08:51:30 +0100

    Temporal changes

--- providentia/filter.py
+++ providentia/filter.py
@@ -64,27 +64,31 @@ class DataFilter:
             # iterate through all species to filter by
             for filter_networkspeci, speci_limits in self.read_instance.filter_species.items():
 
-                # get lower and upper limits for species
-                lower_limit = speci_limits[0]
-                upper_limit = speci_limits[1]
-                filter_species_fill_value = speci_limits[2]
-
                 # get where data is outside bounds, or NaN
-                if filter_networkspeci in self.read_instance.networkspecies:
-                    invalid_inds_per_species = np.logical_or.reduce((self.read_instance.data_in_memory_filtered[filter_networkspeci][self.obs_index, :,:] < lower_limit,
-                                                                     self.read_instance.data_in_memory_filtered[filter_networkspeci][self.obs_index, :,:] > upper_limit,
-                                                                     np.isnan(self.read_instance.data_in_memory_filtered[filter_networkspeci][self.obs_index, :,:])))
-                else:
-                    invalid_inds_per_species = np.logical_or.reduce((self.read_instance.filter_data_in_memory[filter_networkspeci][:,:] < lower_limit,
-                                                                     self.read_instance.filter_data_in_memory[filter_networkspeci][:,:] > upper_limit,
-                                                                     np.isnan(self.read_instance.filter_data_in_memory[filter_networkspeci][:,:])))
+                for speci_limit in speci_limits:
 
-                # update inds_to_filter array, making True all instances where have data outside bounds
-                inds_to_filter = np.any([inds_to_filter, invalid_inds_per_species], axis=0)
+                    # get lower and upper limits for species
+                    lower_limit = speci_limit[0]
+                    upper_limit = speci_limit[1]
+                    filter_species_fill_value = speci_limit[2]
 
-            # set all inds to filter as NaN for all networkspecies in memory
-            for networkspeci in self.read_instance.networkspecies:
-                self.read_instance.data_in_memory_filtered[networkspeci][self.obs_index, inds_to_filter] = filter_species_fill_value      
+                    # get condition
+                    if filter_networkspeci in self.read_instance.networkspecies:
+                        filter_conditions = (self.read_instance.data_in_memory_filtered[filter_networkspeci][self.obs_index, :,:] < lower_limit,
+                                             self.read_instance.data_in_memory_filtered[filter_networkspeci][self.obs_index, :,:] > upper_limit,
+                                             np.isnan(self.read_instance.data_in_memory_filtered[filter_networkspeci][self.obs_index, :,:]))
+                    else:
+                        filter_conditions = (self.read_instance.filter_data_in_memory[filter_networkspeci][:,:] < lower_limit,
+                                             self.read_instance.filter_data_in_memory[filter_networkspeci][:,:] > upper_limit,
+                                             np.isnan(self.read_instance.filter_data_in_memory[filter_networkspeci][:,:]))
+                    
+                    # update inds_to_filter array, making True all instances where have data outside bounds
+                    invalid_inds_per_species = np.logical_or.reduce(filter_conditions)
+                    inds_to_filter = np.any([inds_to_filter, invalid_inds_per_species], axis=0)
+
+                    # set all inds to filter as NaN for all networkspecies in memory
+                    for networkspeci in self.read_instance.networkspecies:
+                        self.read_instance.data_in_memory_filtered[networkspeci][self.obs_index, inds_to_filter] = filter_species_fill_value      
                 
     def filter_data_limits(self):
         """ Filter out (set to NaN) data which exceed the lower/upper limits. """
