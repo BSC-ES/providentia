36e42a55a1938308ce014ce710e5609d499890e3
Dene Bowdalo <denebowdalo@googlemail.com>
2023-03-27 09:40:17 +0200

    optimised lasso

--- providentia/dashboard_aux.py
+++ providentia/dashboard_aux.py
@@ -6,13 +6,15 @@ import time
 import json
 from textwrap import wrap
 import numpy as np
+import matplotlib
+from matplotlib.widgets import _SelectorWidget
+from matplotlib.lines import Line2D
 from PyQt5 import QtCore, QtWidgets, QtGui
 from functools import partial
 from .read_aux import get_default_qa
 
 CURRENT_PATH = os.path.abspath(os.path.dirname(__file__))
 formatting_dict = json.load(open(os.path.join(CURRENT_PATH, 'conf/stylesheet.json')))
-
 
 def set_formatting(PyQt5_obj, format):
     """ Function that takes a PyQt5 object and applies some defined formatting. """
@@ -102,6 +104,80 @@ def wrap_tooltip_text(tooltip_text, max_width):
         tooltip_text = '\n'.join(wrap(tooltip_text, max_char))
 
     return tooltip_text
+
+class LassoSelector(_SelectorWidget):
+    """
+    Selection curve of an arbitrary shape.
+    For the selector to remain responsive you must keep a reference to it.
+    The selected path can be used in conjunction with `~.Path.contains_point`
+    to select data points from an image.
+    In contrast to `Lasso`, `LassoSelector` is written with an interface
+    similar to `RectangleSelector` and `SpanSelector`, and will continue to
+    interact with the Axes until disconnected.
+    Example usage::
+        ax = plt.subplot()
+        ax.plot(x, y)
+        def onselect(verts):
+            print(verts)
+        lasso = LassoSelector(ax, onselect)
+    Parameters
+    ----------
+    ax : `~matplotlib.axes.Axes`
+        The parent Axes for the widget.
+    onselect : function
+        Whenever the lasso is released, the *onselect* function is called and
+        passed the vertices of the selected path.
+    useblit : bool, default: True
+        Whether to use blitting for faster drawing (if supported by the
+        backend). See the tutorial :doc:`/tutorials/advanced/blitting`
+        for details.
+    props : dict, optional
+        Properties with which the line is drawn, see `matplotlib.lines.Line2D`
+        for valid properties. Default values are defined in ``mpl.rcParams``.
+    button : `.MouseButton` or list of `.MouseButton`, optional
+        The mouse buttons used for rectangle selection.  Default is ``None``,
+        which corresponds to all buttons.
+    """
+
+    def __init__(self, canvas_instance, ax, onselect, useblit=True, props=None, button=None):
+        super().__init__(ax, onselect, useblit=useblit, button=button)
+        self.verts = None
+        props = {
+            **(props if props is not None else {}),
+            # Note that self.useblit may be != useblit, if the canvas doesn't
+            # support blitting.
+            'animated': self.useblit, 'visible': False,
+        }
+        line = Line2D([], [], **props)
+        self.ax.add_line(line)
+        self._selection_artist = line
+        self.canvas_instance = canvas_instance
+
+    def _press(self, event):
+        self.verts = [self._get_data(event)]
+        self._selection_artist.set_visible(True)
+
+    def _release(self, event):
+        if self.verts is not None:
+            self.verts.append(self._get_data(event))
+            self.onselect(self.verts)
+        self._selection_artist.set_data([[], []])
+        self._selection_artist.set_visible(False)
+        self.verts = None
+        if self.canvas_instance.map_annotation_disconnect:
+            self.canvas_instance.map_annotation_event = self.canvas_instance.figure.canvas.mpl_connect('motion_notify_event', self.canvas_instance.hover_map_annotation)
+            self.canvas_instance.map_annotation_disconnect = False
+
+    def _onmove(self, event):
+        if not self.canvas_instance.map_annotation_disconnect:
+            self.canvas_instance.figure.canvas.mpl_disconnect(self.canvas_instance.map_annotation_event)
+            self.canvas_instance.map_annotation_disconnect = True
+        if self.verts is None:
+            return
+        self.verts.append(self._get_data(event))
+        self._selection_artist.set_data(list(zip(*self.verts)))
+
+        self.update()
 
 class ComboBox(QtWidgets.QComboBox):
     """ Modify default class of PyQT5 combobox to always dropdown from fixed
