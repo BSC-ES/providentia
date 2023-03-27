e4ca1e679424608acbcca6ac95ac9763a1124a7a
Alba Vilanova <alba.vilanova@bsc.es>
2023-03-27 14:42:24 +0200

    Fix implementation of multispecies filtering

--- providentia/read.py
+++ providentia/read.py
@@ -67,7 +67,7 @@ class DataReader:
                 self.read_instance.invalid_read = True
                 msg = 'Extend the time range or enhance the resolution (e.g. from monthly to daily) to create plots. '
                 msg += 'Plots will only be created when period is longer than 2 timesteps.'
-                show_message(msg, offline=self.read_instance.offline, from_conf=self.read_instance.from_conf)
+                show_message(self.read_instance, msg, from_conf=self.read_instance.from_conf)
                 if (self.read_instance.from_conf) and (not self.read_instance.offline):
                     sys.exit('Error: Providentia will not be launched.')
                 elif (self.read_instance.offline):
@@ -89,7 +89,7 @@ class DataReader:
                         if networkspeci in self.read_instance.networkspecies:
                             msg = 'The current network-species ({}) cannot be selected as a filter species. '.format(networkspeci)
                             msg += 'If you want to change its data bounds, use the lower and upper bounds parameters.'
-                            show_message(msg, offline=self.read_instance.offline, from_conf=self.read_instance.from_conf)
+                            show_message(self.read_instance, msg, from_conf=self.read_instance.from_conf)
                             del self.read_instance.filter_species[networkspeci]
 
                 # get yearmonths in data range (incomplete months are removed for monthly resolution)
