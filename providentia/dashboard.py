8e876e7f8e4e58ddc78462043c28380f7df3f09c
Alba Vilanova <alba.vilanova@bsc.es>
2023-03-28 15:41:17 +0200

    Final edits to new implementation of multispecies filtering

--- providentia/dashboard.py
+++ providentia/dashboard.py
@@ -2,10 +2,8 @@
 from .configuration import ProvConfiguration
 from .canvas import MPLCanvas
 from .toolbar import NavigationToolbar
-from .toolbar import multispecies_conf
 from .dashboard_aux import ComboBox, QVLine, Switch, PopUpWindow, InputDialog
 from .dashboard_aux import set_formatting
-from .aux import show_message
 from .read import DataReader
 from .read_aux import get_default_qa
 from providentia import aux
@@ -439,7 +437,7 @@ class ProvidentiaMainWindow(QtWidgets.QWidget):
             self.handle_data_selection_update()
 
             # set filtered multispecies if any
-            multispecies_conf(self)
+            aux.multispecies_conf(self)
 
             # set fields available for filtering
             aux.representativity_conf(self)
@@ -1040,7 +1038,7 @@ class ProvidentiaMainWindow(QtWidgets.QWidget):
         if (self.filter_species) and (not self.spatial_colocation):
             self.filter_species = {} 
             msg = '"spatial_colocation" must be set to True if wanting to use "filter_species" option.'
-            show_message(self.read_instance, msg)
+            aux.show_message(self.read_instance, msg)
 
         # set read operations to be empty list initially
         read_operations = []
