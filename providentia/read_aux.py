229fe3e64c7a45c4bf3373a14f5f16d1ed280870
Alba Vilanova <alba.vilanova@outlook.com>
2023-04-19 11:33:17 +0200

    Revert change to cftime version

--- providentia/read_aux.py
+++ providentia/read_aux.py
@@ -70,9 +70,8 @@ def read_netcdf_data(tuple_arguments):
     else:
         file_time = num2date(ncdf_root['time'][:], time_units)
         # remove microseconds and convert to integer
-        print(file_time)
         file_time = pd.to_datetime([t.replace(microsecond=0) for t in file_time])
-    
+
     # get file time as integer timestamp
     file_timestamp = file_time.asi8
 
