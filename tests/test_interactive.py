b0a368ffc25aa353118d550e46c7715351c444a5
Alba Vilanova Cortezon <alba.vilanova@outlook.com>
2024-01-12 15:38:28 +0100

    Update files for pipelines

--- tests/test_interactive.py
+++ tests/test_interactive.py
@@ -2,15 +2,14 @@
 from providentia import Interactive
 
 # read and filter data from .conf
-inst = Interactive(conf='test_interactive.conf')
+inst = Interactive(conf='test_local.conf')
 
 # save data to netCDF
 inst.save(format='nc')
 
 # return data in memory (in netCDF format)
 data = inst.get_data(format='nc')
-print(data)
 
 # make a Providentia plot 
 ax = inst.make_plot('timeseries')
-print(ax.get_xydata())
+print('Test is finished')
