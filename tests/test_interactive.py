6bd6d9423b926d1bab71066a5ac766bdfb45471c
Alba Vilanova <alba.vilanova@bsc.es>
2024-01-12 16:36:04 +0100

    Add read data and timeseries unit tests

--- tests/test_interactive.py
+++ tests/test_interactive.py
@@ -1,15 +1,49 @@
-# load Providentia interactive class
+import matplotlib
+import numpy as np
+
 from providentia import Interactive
 
-# read and filter data from .conf
+# initialise class
 inst = Interactive(conf='test_local.conf')
 
-# save data to netCDF
-inst.save(format='nc')
 
-# return data in memory (in netCDF format)
-data = inst.get_data(format='nc')
+def test_data_read():
 
-# make a Providentia plot 
-ax = inst.make_plot('timeseries')
-print('Test is finished')
+    # read expected output
+    expected_output = np.load('tests/data/EBAS_sconco3_data.npy', allow_pickle=True)
+    
+    # get data in memory in xarray format
+    data = inst.get_data(format='xr')
+    generated_output = data['EBAS|sconco3_data'].values
+    # np.save('tests/data/EBAS_sconco3_data', generated_output)
+
+    # replace nans by -999
+    expected_output = np.nan_to_num(expected_output, copy=True, nan=-999, posinf=None, neginf=None)
+    generated_output = np.nan_to_num(generated_output, copy=True, nan=-999, posinf=None, neginf=None)
+
+    assert (np.array_equal(generated_output, expected_output))
+
+def test_make_timeseries():
+
+    # make timeseries 
+    ax = inst.make_plot('timeseries', return_plot=True)
+
+    # check that an axis has been returned
+    assert (type(ax) == matplotlib.axes._axes.Axes)
+
+    for line_i, line in enumerate(ax.lines):
+
+        # np.save(f'tests/data/timeseries_line_{line_i}', line.get_xydata())
+
+        # read expected output
+        expected_output = np.load(f'tests/data/timeseries_line_{line_i}.npy', allow_pickle=True)
+
+        # get data in timeseries
+        generated_output = line.get_xydata()
+
+        # replace nans by -999
+        expected_output = np.nan_to_num(expected_output, copy=True, nan=-999, posinf=None, neginf=None)
+        generated_output = np.nan_to_num(generated_output, copy=True, nan=-999, posinf=None, neginf=None)
+
+        # check data for each timeseries line is correct
+        assert (np.array_equal(generated_output, expected_output))
