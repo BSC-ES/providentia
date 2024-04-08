1de558d08f88b369eefe2200e073e739f0b021c8
Alba Vilanova Cortezon <alba.vilanova@outlook.com>
2024-04-08 15:42:54 +0200

    Update tests per mode

--- tests/test_local_flattened.py
+++ tests/test_local_flattened.py
@@ -1,9 +1,9 @@
 from .aux import read_data, make_plot
 from providentia import Interactive
 
-# initialise class (Temporal|Spatial)
-mode = "temporal_spatial"
-inst = Interactive(conf='local.conf')
+# initialise class (Flattened)
+mode = "flattened"
+inst = Interactive(conf='local.conf', statistic_mode="Flattened", statistic_aggregation="")
 expected_annotations = {
     "timeseries": ["EBAS | Mean: 32.91", "MONARCH | Mean: 27.88, MB: -5.45, RMSE: 10.97, r: 0.68"],
     "distribution": ["EBAS | Min: 1.78, Max: 80.38", "MONARCH | Min: 0.22, Max: 69.92"],
