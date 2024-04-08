a9d942feee5c474709da58b7ae93ba8192ec6db6
Alba Vilanova <alba.vilanova@outlook.com>
2024-04-08 15:37:06 +0200

    Divide tests to allow for other config files

--- tests/aux.py
+++ tests/aux.py
@@ -1,18 +1,8 @@
 import matplotlib
 import numpy as np
 
-from providentia import Interactive
 
-# initialise class
-inst = Interactive(conf='local.conf')
-
-expected_annotations = {
-    "timeseries": ["EBAS | Mean: 32.91", "MONARCH | Mean: 27.88, MB: -5.45, RMSE: 10.97, r: 0.68"],
-    "distribution": ["EBAS | Min: 1.78, Max: 80.38", "MONARCH | Min: 0.22, Max: 69.92"],
-    "scatter": ["MONARCH | r2: 0.46, RMSE: 10.97"]
-}
-
-def test_data_read():
+def read_data(inst):
 
     # read expected output
     expected_output = np.load('tests/data/EBAS_sconco3_data.npy', allow_pickle=True)
@@ -28,7 +18,8 @@ def test_data_read():
 
     assert (np.array_equal(generated_output, expected_output))
 
-def make_plot(plot_type):
+
+def make_plot(inst, plot_type, expected_annotations):
 
     # make plot
     fig = inst.make_plot(plot_type, annotate=True, return_plot=True)
@@ -46,7 +37,7 @@ def make_plot(plot_type):
 
     for line_i, line in enumerate(fig.axes[0].lines):
         
-        #np.save(f'tests/data/{plot_type}_line_{line_i}', line.get_xydata())
+        # np.save(f'tests/data/{plot_type}_line_{line_i}', line.get_xydata())
             
         # read expected output
         expected_output = np.load(f'tests/data/{plot_type}_line_{line_i}.npy', allow_pickle=True)
@@ -62,12 +53,3 @@ def make_plot(plot_type):
 
         # check data for each line is correct
         assert (np.array_equal(generated_output, expected_output))
-
-def test_make_timeseries():
-    make_plot('timeseries')
-
-def test_make_distribution():
-    make_plot('distribution')
-
-#def test_make_scatter():
-#    make_plot('scatter')
