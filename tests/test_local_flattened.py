08ccd4f75cb0a5f164311edf850f984a68e5c998
Alba Vilanova Cortezon <alba.vilanova@outlook.com>
2024-04-08 15:45:53 +0200

    Replace mode by test_name

--- tests/test_local_flattened.py
+++ tests/test_local_flattened.py
@@ -2,8 +2,8 @@ from .aux import read_data, make_plot
 from providentia import Interactive
 
 # initialise class (Flattened)
-mode = "flattened"
-inst = Interactive(conf='local.conf', statistic_mode="Flattened", statistic_aggregation="")
+test_name = "local_flattened"
+inst = Interactive(conf='local.conf', statistic_test_name="Flattened", statistic_aggregation="")
 expected_annotations = {
     "timeseries": ["EBAS | Mean: 32.91", "MONARCH | Mean: 27.88, MB: -5.45, RMSE: 10.97, r: 0.68"],
     "distribution": ["EBAS | Min: 1.78, Max: 80.38", "MONARCH | Min: 0.22, Max: 69.92"],
@@ -11,13 +11,13 @@ expected_annotations = {
 }
 
 def test_read_data():
-    read_data(inst, mode)
+    read_data(inst, test_name)
 
 def test_make_timeseries():
-    make_plot(inst, 'timeseries', expected_annotations, mode)
+    make_plot(inst, 'timeseries', expected_annotations, test_name)
 
 def test_make_distribution():
-    make_plot(inst, 'distribution', expected_annotations, mode)
+    make_plot(inst, 'distribution', expected_annotations, test_name)
 
 # def test_make_scatter():
 #    make_plot('scatter')
