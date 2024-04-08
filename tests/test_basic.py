b5df9948c7dc69bab4700600f780d7c05b713dc1
Alba Vilanova Cortezon <alba.vilanova@outlook.com>
2024-04-08 15:00:58 +0200

    Generalise make plot test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -6,6 +6,9 @@ from providentia import Interactive
 # initialise class
 inst = Interactive(conf='local.conf')
 
+expected_annotations = {
+    "timeseries": ['EBAS | Mean: 32.91', 'MONARCH | Mean: 27.88, MB: -5.45, RMSE: 10.97, r: 0.68']
+}
 
 def test_data_read():
 
@@ -23,35 +26,39 @@ def test_data_read():
 
     assert (np.array_equal(generated_output, expected_output))
 
-def test_make_timeseries():
+def make_plot(plot_type):
 
-    # make timeseries
-    fig = inst.make_plot('timeseries', annotate=True, return_plot=True)
+    # make plot
+    fig = inst.make_plot(plot_type, annotate=True, return_plot=True)
 
-    # check that an axis has been returned
+    # check that a figure has been returned
     assert (type(fig) == matplotlib.figure.Figure)
 
-
     # check if annotations are correct
-    annotations = [child for child in fig.axes[0].get_children() if type(child) == matplotlib.offsetbox.AnchoredOffsetbox][0]
-    expected_annotations = ['EBAS | Mean: 32.91', 'MONARCH | Mean: 27.88, MB: -5.45, RMSE: 10.97, r: 0.68']
-    for annotation, expected_annotation in zip(annotations.get_child().get_children(), expected_annotations):
+    annotations = [child for child in fig.axes[0].get_children() 
+                   if type(child) == matplotlib.offsetbox.AnchoredOffsetbox][0]
+    for annotation, expected_annotation in zip(annotations.get_child().get_children(), 
+                                               expected_annotations['timeseries']):
         assert annotation.get_text() == expected_annotation
 
     for line_i, line in enumerate(fig.axes[0].lines):
 
-        #np.save(f'tests/data/timeseries_line_{line_i}', line.get_xydata())
+        #np.save(f'tests/data/{plot_type}_line_{line_i}', line.get_xydata())
 
         # read expected output
-        expected_output = np.load(f'tests/data/timeseries_line_{line_i}.npy', allow_pickle=True)
+        expected_output = np.load(f'tests/data/{plot_type}_line_{line_i}.npy', allow_pickle=True)
 
-        # get data in timeseries
+        # get data
         generated_output = line.get_xydata()
 
         # replace nans by -999
-        expected_output = np.nan_to_num(expected_output, copy=True, nan=-999, posinf=None, neginf=None)
-        generated_output = np.nan_to_num(generated_output, copy=True, nan=-999, posinf=None, neginf=None)
+        expected_output = np.nan_to_num(expected_output, copy=True, nan=-999, posinf=None, 
+                                        neginf=None)
+        generated_output = np.nan_to_num(generated_output, copy=True, nan=-999, posinf=None, 
+                                         neginf=None)
 
-        # check data for each timeseries line is correct
+        # check data for each line is correct
         assert (np.array_equal(generated_output, expected_output))
 
+def test_make_timeseries():
+    make_plot('timeseries')
