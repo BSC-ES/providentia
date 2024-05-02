ded5bb82be6906d4aa9f50d88f9eea0bb46756d3
Paula Serrano Sierra <paulaserranosierra@gmail.com>
2024-05-02 16:40:25 +0200

    Deletion of mn4 and power9 in bin file. Merged both load_modules into providentia one. Working on nord3v2.

--- load_modules.sh
+++ load_modules.sh
@@ -1,31 +1,9 @@
 #!/bin/sh
 module purge
 
-# CTE Power9
-if [ "${BSC_MACHINE}" == "power" ]; then
-    module load Python/3.7.0-foss-2018b \
-        Cartopy/0.20.3-foss-2018b-Python-3.7.0 \
-        cftime/1.0.3.4-foss-2018b-Python-3.7.0 \
-        matplotlib/3.1.1-foss-2018b-Python-3.7.0 \
-        netcdf4-python/1.5.1.2-foss-2018b-Python-3.7.0 \
-        numpy/1.16.4-foss-2018b-Python-3.7.0 \
-        pandas/0.24.2-foss-2018b-Python-3.7.0 \
-        PyQt5/5.13.0-foss-2018b-Python-3.7.0 \
-        seaborn/0.11.1-foss-2018b-Python-3.7.0 \
-        scipy/1.3.0-foss-2018b-Python-3.7.0 \
-        kdepy/1.1.1-fosscuda-2019b-Python-3.7.4 \
-        Ghostscript/9.50-GCCcore-8.3.0
-
 # Marenostrum5
-elif [ "${BSC_MACHINE}" == "mn5" ]; then
+if [ "${BSC_MACHINE}" == "mn5" ]; then
     module load anaconda/2023.07
-
-# Marenostrum4
-elif [ "${BSC_MACHINE}" == "mn4" ]; then
-    module load intel impi mkl \
-        gcc/5.4.0 python/3.7.4_ES_test_jupyterlab \
-        sqlite/3.41.0 proj/8.2.1 hdf5 netcdf \
-        geos/3.10.2 gdal/3.6.2 libtiff
 
 # Nord3v2
 elif [ "${BSC_MACHINE}" == "nord3v2" ] || [ "${BSC_MACHINE}" = "amd" ]; then 
@@ -43,7 +21,14 @@ elif [ "${BSC_MACHINE}" == "nord3v2" ] || [ "${BSC_MACHINE}" = "amd" ]; then
         OpenMPI/4.0.5-GCC-8.3.0-nord3-v2 \
         pyproj/3.2.1-foss-2019b-Python-3.7.4 \
         kdepy/1.1.1-foss-2019b-Python-3.7.4 \
-        jupyterlab/3.0.9-foss-2019b-Python-3.7.4
+        jupyterlab/3.0.9-foss-2019b-Python-3.7.4 \
+        UDUNITS/2.2.26-GCCcore-8.3.0 \
+        HDF5/1.10.5-gompi-2019b \
+        netCDF/4.7.1-gompi-2019b \
+        GEOS/3.7.2-foss-2019b-Python-3.7.4 \
+        GSL/2.6-GCC-8.3.0 \
+        NCO/4.9.2-foss-2019b \
+        greasy/2.2.3 
 
 # Hub
 elif [ "${ip}" == "84.88.185.48" ]; then
