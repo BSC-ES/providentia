0b610d9a4b9ddda016154b4c30d087ca2d4b4be8
Alba Vilanova Cortezon <alba.vilanova@outlook.com>
2024-06-11 15:51:50 +0200

    Update cartopy dir

--- providentia/configuration.py
+++ providentia/configuration.py
@@ -178,8 +178,10 @@ class ProvConfiguration:
         elif key == 'cartopy_data_dir':
             # set cartopy data directory (needed on CTE-POWER/MN4/Nord3v2 as has no external
             # internet connection)
-            if MACHINE in ['power', 'mn4', 'nord3v2', 'mn5']:
+            if MACHINE in ['power', 'mn4', 'nord3v2']:
                 return '/gpfs/projects/bsc32/software/rhel/7.5/ppc64le/POWER9/software/Cartopy/0.17.0-foss-2018b-Python-3.7.0/lib/python3.7/site-packages/Cartopy-0.17.0-py3.7-linux-ppc64le.egg/cartopy/data'
+            elif MACHINE in ['mn5']:
+                return '/gpfs/projects/bsc32/software/rhel/9.2/software/Cartopy/0.23.0-foss-2023b-Python-3.11.5/lib/python3.11/site-packages/cartopy/data'
             # on all other machines pull from internet
 
         elif key == 'n_cpus':
