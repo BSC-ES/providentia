7e09c52de7dff958dc4b9575fba5cc06e0a3ad1b
Alba Vilanova <alba.vilanova@outlook.com>
2024-08-01 15:05:06 +0200

    Use more stats and include bias statsummary

--- tests/test_local_temporal_spatial.py
+++ tests/test_local_temporal_spatial.py
@@ -9,14 +9,27 @@ expected_annotations = {
     "distribution": ["EBAS | Min: 1.78, Max: 80.38", "MONARCH | Min: 0.22, Max: 69.92"],
 }
 
+
 def test_read_data():
     read_data(inst, test_name)
 
+
 def test_make_timeseries():
-    make_plot(inst, test_name, 'timeseries', ['annotate'], expected_annotations)
+    make_plot(inst, test_name, 'timeseries', [
+              'annotate'], expected_annotations)
+
 
 def test_make_distribution():
-    make_plot(inst, test_name, 'distribution', ['annotate'], expected_annotations)
+    make_plot(inst, test_name, 'distribution', [
+              'annotate'], expected_annotations)
+
 
 def test_make_statsummary():
-    make_plot(inst, test_name, 'statsummary')
+    stats = ["Mean", "StdDev", "Median", "Var", "Min", "Max",
+             "NData", "Data%", "Exceedances", "p1", "p5", "p10",
+             "p25", "p75", "p90", "p95", "p99"]
+    make_plot(inst, test_name, 'statsummary', stats=stats)
+
+    stats = ["MB", "NMB", "ME", "NME", "MNB", "MNE", "MFB",
+             "MFE", "RMSE", "NRMSE", "COE", "FAC2", "IOA", "r", "r2", "UPA"]
+    make_plot(inst, test_name, 'statsummary', ['bias'], stats=stats)
