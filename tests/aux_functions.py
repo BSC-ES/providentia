1e7f2379048b67e52fad44cbae11edd78377a91f
Alba Vilanova <alba.vilanova@bsc.es>
2024-08-01 12:50:13 +0200

    Update tests

--- tests/aux_functions.py
+++ tests/aux_functions.py
@@ -1,5 +1,7 @@
 import matplotlib
+import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 def read_data(inst, test_name):
@@ -7,10 +9,12 @@ def read_data(inst, test_name):
     # get data in memory in xarray format
     data = inst.get_data(format='xr')
     generated_output = data['EBAS|sconco3_data'].values
-    #np.save(f'tests/data/{test_name}/EBAS_sconco3_data', generated_output)
+
+    # save data, uncomment if we want to update it
+    # np.save(f'tests/reference/{test_name}/data/EBAS_sconco3_data', generated_output)
 
     # read expected output
-    expected_output = np.load(f'tests/data/{test_name}/EBAS_sconco3_data.npy', allow_pickle=True)
+    expected_output = np.load(f'tests/reference/{test_name}/data/EBAS_sconco3_data.npy', allow_pickle=True)
     
     assert (np.allclose(generated_output, expected_output, equal_nan=True))
 
@@ -23,23 +27,54 @@ def make_plot(inst, test_name, plot_type, plot_options=[], expected_annotations=
     # check that a figure has been returned
     assert (type(fig) == matplotlib.figure.Figure)
 
-    # check if annotations are correct
-    annotations = [child for child in fig.axes[0].get_children() 
-                   if type(child) == matplotlib.offsetbox.AnchoredOffsetbox][0]
-    
-    for annotation, expected_annotation in zip(annotations.get_child().get_children(), 
-                                               expected_annotations[plot_type]):
-        assert annotation.get_text() == expected_annotation
+    if plot_type in ['statsummary']:
 
-    for line_i, line in enumerate(fig.axes[0].lines):
-        
-        #np.save(f'tests/data/{test_name}/{plot_type}_line_{line_i}', line.get_xydata())
+        # get table
+        for artist in fig.axes[0].get_children():
+            if isinstance(artist, matplotlib.table.Table):
+                table = artist
+                break
             
-        # read expected output
-        expected_output = np.load(f'tests/data/{test_name}/{plot_type}_line_{line_i}.npy', allow_pickle=True)
+        # extract data from the table
+        data = []
+        for (row, col), cell in table.get_celld().items():
+            data.append({
+                "row": row,
+                "col": col,
+                "value": cell.get_text().get_text()
+            })
+        generated_output = pd.DataFrame(data)
 
-        # get data
-        generated_output = line.get_xydata()
+        # save data, uncomment if we want to update it
+        # generated_output.to_csv(f'tests/reference/{test_name}/{plot_type}/{plot_type}_table_values.csv', index=False)
 
-        # check data for each line is correct
-        assert (np.allclose(generated_output, expected_output, equal_nan=True))
+        expected_output = pd.read_csv(
+            f'tests/reference/{test_name}/{plot_type}/{plot_type}_table_values.csv', keep_default_na=False)
+
+        assert generated_output.equals(expected_output)
+
+    elif plot_type in ['timeseries', 'distribution']:
+        
+        # check if annotations are correct
+        if 'annotate' in plot_options and expected_annotations:
+            annotations = [child for child in fig.axes[0].get_children() 
+                        if type(child) == matplotlib.offsetbox.AnchoredOffsetbox][0]
+            
+            for annotation, expected_annotation in zip(annotations.get_child().get_children(), 
+                                                    expected_annotations[plot_type]):
+                assert annotation.get_text() == expected_annotation
+        
+        # iterate through plotted lines
+        for line_i, line in enumerate(fig.axes[0].lines):
+            
+            # get data from line
+            generated_output = line.get_xydata()
+
+            # save data, uncomment if we want to update it
+            # np.save(f'tests/reference/{test_name}/{plot_type}/{plot_type}_line_{line_i}', generated_output)
+                
+            # read expected output
+            expected_output = np.load(f'tests/reference/{test_name}/{plot_type}/{plot_type}_line_{line_i}.npy', allow_pickle=True)
+
+            # check data for each line is correct
+            assert (np.allclose(generated_output, expected_output, equal_nan=True))
