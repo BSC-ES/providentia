b234aff244c483cb0591935463ad35c372c4c807
Alba Vilanova <alba.vilanova@outlook.com>
2024-08-01 12:50:13 +0200

    Update tests

--- tests/test_local_temporal_spatial.py
+++ tests/test_local_temporal_spatial.py
@@ -7,7 +7,6 @@ inst = Interactive(conf='local.conf')
 expected_annotations = {
     "timeseries": ["EBAS | Mean: 32.91", "MONARCH | Mean: 27.88, MB: -5.45, RMSE: 10.97, r: 0.68"],
     "distribution": ["EBAS | Min: 1.78, Max: 80.38", "MONARCH | Min: 0.22, Max: 69.92"],
-    "scatter": [""]
 }
 
 def test_read_data():
@@ -18,3 +17,6 @@ def test_make_timeseries():
 
 def test_make_distribution():
     make_plot(inst, test_name, 'distribution', ['annotate'], expected_annotations)
+
+def test_make_statsummary():
+    make_plot(inst, test_name, 'statsummary')
