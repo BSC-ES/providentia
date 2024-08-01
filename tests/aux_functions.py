6df3957130393b96a6cee1a9e261d81ea7a6c118
Alba Vilanova <alba.vilanova@outlook.com>
2024-08-01 15:05:06 +0200

    Use more stats and include bias statsummary

--- tests/aux_functions.py
+++ tests/aux_functions.py
@@ -2,6 +2,7 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+from providentia.statistics import get_z_statistic_info
 
 
 def read_data(inst, test_name):
@@ -11,30 +12,42 @@ def read_data(inst, test_name):
     generated_output = data['EBAS|sconco3_data'].values
 
     # save data, uncomment if we want to update it
-    # np.save(f'tests/reference/{test_name}/data/EBAS_sconco3_data', generated_output)
+    path = f'tests/reference/{test_name}/data/EBAS_sconco3_data.npy'
+    # np.save(path, generated_output)
 
     # read expected output
-    expected_output = np.load(f'tests/reference/{test_name}/data/EBAS_sconco3_data.npy', allow_pickle=True)
-    
+    expected_output = np.load(path, allow_pickle=True)
+
     assert (np.allclose(generated_output, expected_output, equal_nan=True))
 
 
-def make_plot(inst, test_name, plot_type, plot_options=[], expected_annotations=[]):
+def make_plot(inst, test_name, plot_type, plot_options=[], expected_annotations=[], stats=[]):
 
     # make plot
-    fig = inst.make_plot(plot_type, plot_options=plot_options, return_plot=True)
+    fig = inst.make_plot(plot_type, plot_options=plot_options,
+                         return_plot=True, stats=stats)
 
     # check that a figure has been returned
     assert (type(fig) == matplotlib.figure.Figure)
 
-    if plot_type in ['statsummary']:
+    # get zstat information from plot_type
+    zstat, base_zstat, z_statistic_type, z_statistic_sign, z_statistic_period = get_z_statistic_info(
+        plot_type=plot_type)
+
+    # get base plot type (without stat and options)
+    if zstat:
+        base_plot_type = plot_type.split('-')[0]
+    else:
+        base_plot_type = plot_type.split('_')[0]
+
+    if base_plot_type in ['statsummary']:
 
         # get table
         for artist in fig.axes[0].get_children():
             if isinstance(artist, matplotlib.table.Table):
                 table = artist
                 break
-            
+
         # extract data from the table
         data = []
         for (row, col), cell in table.get_celld().items():
@@ -46,35 +59,40 @@ def make_plot(inst, test_name, plot_type, plot_options=[], expected_annotations=
         generated_output = pd.DataFrame(data)
 
         # save data, uncomment if we want to update it
-        # generated_output.to_csv(f'tests/reference/{test_name}/{plot_type}/{plot_type}_table_values.csv', index=False)
+        if 'bias' in plot_options:
+            path = f'tests/reference/{test_name}/{plot_type}/{plot_type}_bias_table_values.csv'
+        else:
+            path = f'tests/reference/{test_name}/{plot_type}/{plot_type}_table_values.csv'
+        generated_output.to_csv(path, index=False)
 
-        expected_output = pd.read_csv(
-            f'tests/reference/{test_name}/{plot_type}/{plot_type}_table_values.csv', keep_default_na=False)
+        expected_output = pd.read_csv(path, keep_default_na=False)
 
         assert generated_output.equals(expected_output)
 
     elif plot_type in ['timeseries', 'distribution']:
-        
+
         # check if annotations are correct
         if 'annotate' in plot_options and expected_annotations:
-            annotations = [child for child in fig.axes[0].get_children() 
-                        if type(child) == matplotlib.offsetbox.AnchoredOffsetbox][0]
-            
-            for annotation, expected_annotation in zip(annotations.get_child().get_children(), 
-                                                    expected_annotations[plot_type]):
+            annotations = [child for child in fig.axes[0].get_children()
+                           if type(child) == matplotlib.offsetbox.AnchoredOffsetbox][0]
+
+            for annotation, expected_annotation in zip(annotations.get_child().get_children(),
+                                                       expected_annotations[plot_type]):
                 assert annotation.get_text() == expected_annotation
-        
+
         # iterate through plotted lines
         for line_i, line in enumerate(fig.axes[0].lines):
-            
+
             # get data from line
             generated_output = line.get_xydata()
 
             # save data, uncomment if we want to update it
-            # np.save(f'tests/reference/{test_name}/{plot_type}/{plot_type}_line_{line_i}', generated_output)
-                
+            path = f'tests/reference/{test_name}/{plot_type}/{plot_type}_line_{line_i}.npy'
+            # np.save(path, generated_output)
+
             # read expected output
-            expected_output = np.load(f'tests/reference/{test_name}/{plot_type}/{plot_type}_line_{line_i}.npy', allow_pickle=True)
+            expected_output = np.load(path, allow_pickle=True)
 
             # check data for each line is correct
-            assert (np.allclose(generated_output, expected_output, equal_nan=True))
+            assert (np.allclose(generated_output,
+                    expected_output, equal_nan=True))
