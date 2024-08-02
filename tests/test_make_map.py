5a8164c218c094a416613d5d4baf0f5a6695822d
Alba Vilanova <alba.vilanova@outlook.com>
2024-08-02 16:56:10 +0200

    Split tests

---
+++ tests/test_make_map.py
@@ -0,0 +1,38 @@
+from .aux_functions import read_data, make_plot
+from providentia import Interactive
+import pytest
+
+
+possibilities = [
+    (Interactive(conf='tests_ghost.conf',
+                 statistic_mode="Flattened",
+                 statistic_aggregation=""),
+     "flattened", "ghost"
+     ),
+    (Interactive(conf='tests_ghost.conf',
+                 statistic_mode="Spatial|Temporal",
+                 statistic_aggregation="Median"),
+     "spatial_temporal", "ghost"
+     ),
+    (Interactive(conf='tests_ghost.conf'),
+     "temporal_spatial", "ghost"
+     ),
+    (Interactive(conf='tests_nonghost.conf',
+                 statistic_mode="Flattened",
+                 statistic_aggregation=""),
+     "flattened", "nonghost"
+     ),
+    (Interactive(conf='tests_nonghost.conf',
+                 statistic_mode="Spatial|Temporal",
+                 statistic_aggregation="Median"),
+     "spatial_temporal", "nonghost",
+     ),
+    (Interactive(conf='tests_nonghost.conf'),
+     "temporal_spatial", "nonghost",
+     )
+]
+
+
+@pytest.mark.parametrize("inst, statistic_mode, network_type", possibilities)
+def test_make_map(inst, statistic_mode, network_type):
+    make_plot(inst, statistic_mode, network_type, 'map-Median', ['domain'])
