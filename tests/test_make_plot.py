9a58afb007a34dc4e3dc9cc9d113bfa4f29e3f8d
Alba Vilanova <alba.vilanova@outlook.com>
2024-08-05 12:03:53 +0200

    Extract annotations and join tests

--- tests/test_make_plot.py
+++ tests/test_make_plot.py
@@ -7,30 +7,37 @@ possibilities = [
     (Interactive(conf='tests_ghost.conf',
                  statistic_mode="Flattened",
                  statistic_aggregation=""),
-     "flattened", "ghost"
-     ),
+     "flattened", "ghost"),
     (Interactive(conf='tests_ghost.conf',
                  statistic_mode="Spatial|Temporal",
                  statistic_aggregation="Median"),
-     "spatial_temporal", "ghost"
-     ),
+     "spatial_temporal", "ghost"),
     (Interactive(conf='tests_ghost.conf'),
-     "temporal_spatial", "ghost"
-     ),
+     "temporal_spatial", "ghost"),
     (Interactive(conf='tests_nonghost.conf',
                  statistic_mode="Flattened",
                  statistic_aggregation=""),
-     "flattened", "nonghost"
-     ),
+     "flattened", "nonghost"),
     (Interactive(conf='tests_nonghost.conf',
                  statistic_mode="Spatial|Temporal",
                  statistic_aggregation="Median"),
-     "spatial_temporal", "nonghost",
-     ),
+     "spatial_temporal", "nonghost"),
     (Interactive(conf='tests_nonghost.conf'),
      "temporal_spatial", "nonghost",
      )
 ]
+
+
+@pytest.mark.parametrize("inst, statistic_mode, network_type", possibilities)
+def test_make_timeseries(inst, statistic_mode, network_type):
+    make_plot(inst, statistic_mode, network_type, 'timeseries', [
+              'annotate', 'smooth'])
+
+
+@pytest.mark.parametrize("inst, statistic_mode, network_type", possibilities)
+def test_make_distribution(inst, statistic_mode, network_type):
+    make_plot(inst, statistic_mode, network_type, 'distribution', [
+              'annotate'])
 
 
 @pytest.mark.parametrize("inst, statistic_mode, network_type", possibilities)
@@ -45,3 +52,15 @@ def test_make_statsummary(inst, statistic_mode, network_type):
                      "MFE", "RMSE", "NRMSE", "COE", "FAC2", "IOA", "r", "r2", "UPA"]
     make_plot(inst, statistic_mode, network_type,
               'statsummary', ['bias'], stats=expbias_stats)
+
+
+@pytest.mark.parametrize("inst, statistic_mode, network_type", possibilities)
+def test_make_map(inst, statistic_mode, network_type):
+    make_plot(inst, statistic_mode, network_type, 'map-Median', [
+              'domain'])
+
+
+@pytest.mark.parametrize("inst, statistic_mode, network_type", possibilities)
+def test_make_taylor(inst, statistic_mode, network_type):
+    make_plot(inst, statistic_mode, network_type,
+              'taylor-r', ['annotate'])
