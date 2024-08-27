290d32b28f22b9c9c419b11204d2833922fc6de0
Paula Serrano Sierra <paulaserranosierra@gmail.com>
2024-08-27 10:08:45 +0200

    Restored nord3 iinterpolation

--- providentia/interpolation/experiment_interpolation_submit.sh
+++ providentia/interpolation/experiment_interpolation_submit.sh
@@ -5,10 +5,6 @@
 #SBATCH --output=providentia/interpolation/submission_logs/%J.out
 #SBATCH --error=/dev/null
 
-echo "Loading modules..."
+JOB_ID="--slurm_job_id=$SLURM_JOB_ID"
 
-# load modules
-source load_modules.sh
-
-srun --output=providentia/interpolation/management_logs/$SLURM_JOB_ID.out python -u providentia/interpolation/experiment_interpolation_submission.py $SLURM_JOB_ID $*
-
+srun --output=providentia/interpolation/management_logs/$SLURM_JOB_ID.out python -u providentia/interpolation/experiment_interpolation_submission.py $@ $JOB_ID
