fb3e832d5207eabc06a2db63d815ecdc4f935e3a
Alba Vilanova <alba.vilanova@outlook.com>
2024-08-27 18:48:49 +0200

    Fix interp in Nord3

--- providentia/interpolation/experiment_interpolation_submit.sh
+++ providentia/interpolation/experiment_interpolation_submit.sh
@@ -7,4 +7,4 @@
 
 JOB_ID="--slurm_job_id=$SLURM_JOB_ID"
 
-srun --output=providentia/interpolation/management_logs/$SLURM_JOB_ID.out python -u providentia/interpolation/experiment_interpolation_submission.py $@ $JOB_ID
+srun --output=providentia/interpolation/management_logs/$SLURM_JOB_ID.out python -u -c "from providentia.main import main; main()" $@ $JOB_ID
