777593fc7aea135338e5795cc9d6cbdab337d300
Alba Vilanova <alba.vilanova@outlook.com>
2024-10-11 11:07:20 +0200

    Move adding annotation to annotate and fix annotations in dashboard

--- providentia/plot_formatting.py
+++ providentia/plot_formatting.py
@@ -525,7 +525,7 @@ def format_plot_options(canvas_instance, read_instance, relevant_axs, relevant_d
 
         # annotation
         if 'annotate' in plot_options:
-            if base_plot_type not in ['heatmap', 'fairmode-target']:
+            if base_plot_type not in ['heatmap']:
                 annotation(canvas_instance, read_instance, relevant_ax, networkspeci, 
                            relevant_data_labels[relevant_ax_ii], base_plot_type, 
                            canvas_instance.plot_characteristics[plot_type],
