db9586b110601ad06971a209a032ebb76c35fd93
Alba Vilanova <alba.vilanova@bsc.es>
2024-12-05 10:39:01 +0100

    Reduce number of CPUs used in interpolation with multiprocessing

--- providentia/auxiliar.py
+++ providentia/auxiliar.py
@@ -17,7 +17,7 @@ def deep_merge(dict1, dict2):
     dict1 : dict
         First dictionary
     dict2 : dict
-        Second dictionary_
+        Second dictionary
     """
     for key, value in dict2.items():
         if key in dict1 and isinstance(dict1[key], dict) and isinstance(value, dict):
