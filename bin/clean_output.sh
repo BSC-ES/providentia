232dc43af934283bc44c197196147ef77de8f2c4
Dene Bowdalo <dene.bowdalo@bsc.es>
2025-05-02 12:31:26 +0200

    corrected clean_output.sh

--- bin/clean_output.sh
+++ bin/clean_output.sh
@@ -2,19 +2,19 @@
 #SBATCH --output=/dev/null
 #SBATCH --error=/dev/null
 
-rm -rf figures/*
-rm -rf plots/*
-rm -rf reports/*
-rm -rf saved_data/*
-rm -rf providentia/__pycache__
-rm -rf notebooks/.ipynb_checkpoints
-rm -f notebook.out
+rm -rf ../figures/*
+rm -rf ../plots/*
+rm -rf ../reports/*
+rm -rf ../saved_data/*
+rm -rf ../providentia/__pycache__
+rm -rf ../notebooks/.ipynb_checkpoints
+rm -f ../notebook.out
 
 # Interpolation
-rm -rf logs/interpolation/arguments/*
-rm -rf logs/interpolation/interpolation_logs/*
-rm -rf logs/interpolation/management_logs/*
-rm -rf logs/interpolation/submission_logs/*
-rm -rf logs/interpolation/greasy_logs/*
-rm -rf logs/interpolation/__pycache__ 
-rm -rf logs/interpolation/slurm*
+rm -rf ../logs/interpolation/arguments/*
+rm -rf ../logs/interpolation/interpolation_logs/*
+rm -rf ../logs/interpolation/management_logs/*
+rm -rf ../logs/interpolation/submission_logs/*
+rm -rf ../logs/interpolation/greasy_logs/*
+rm -rf ../logs/interpolation/__pycache__ 
+rm -rf ../logs/interpolation/slurm*
