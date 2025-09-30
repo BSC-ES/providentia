f27858e59b6c57aa11fc4434e5cc106b6d80f4d8
Alba Vilanova <alba.vilanova@outlook.com>
2025-09-30 15:10:12 +0200

    Update tests

--- tests/test_read_data.py
+++ tests/test_read_data.py
@@ -4,44 +4,44 @@ import pytest
 
 
 possibilities = [
-    (prv.load('tests_ghost.conf',
-              statistic_mode="Flattened",
-              statistic_aggregation="",
-              tests=True),
+    (prv.Providentia('tests_ghost.conf',
+                     statistic_mode="Flattened",
+                     statistic_aggregation="",
+                     tests=True),
      "flattened", "ghost"),
-    (prv.load('tests_ghost.conf',
-              statistic_mode="Spatial|Temporal",
-              statistic_aggregation="Median",
-              tests=True),
+    (prv.Providentia('tests_ghost.conf',
+                     statistic_mode="Spatial|Temporal",
+                     statistic_aggregation="Median",
+                     tests=True),
      "spatial_temporal", "ghost"),
-    (prv.load('tests_ghost.conf',
-              tests=True),
+    (prv.Providentia('tests_ghost.conf',
+                     tests=True),
      "temporal_spatial", "ghost"),
-    (prv.load('tests_nonghost.conf',
-              statistic_mode="Flattened",
-              statistic_aggregation="",
-              tests=True),
+    (prv.Providentia('tests_nonghost.conf',
+                     statistic_mode="Flattened",
+                     statistic_aggregation="",
+                     tests=True),
      "flattened", "nonghost"),
-    (prv.load('tests_nonghost.conf',
-              statistic_mode="Spatial|Temporal",
-              statistic_aggregation="Median",
-              tests=True),
+    (prv.Providentia('tests_nonghost.conf',
+                     statistic_mode="Spatial|Temporal",
+                     statistic_aggregation="Median",
+                     tests=True),
      "spatial_temporal", "nonghost"),
-    (prv.load('tests_nonghost.conf',
-              tests=True),
+    (prv.Providentia('tests_nonghost.conf',
+                     tests=True),
      "temporal_spatial", "nonghost"),
-    (prv.load('tests_nonghost_calibration.conf',
-              statistic_mode="Flattened",
-              statistic_aggregation="",
-              tests=True),
+    (prv.Providentia('tests_nonghost_calibration.conf',
+                     statistic_mode="Flattened",
+                     statistic_aggregation="",
+                     tests=True),
      "flattened", "nonghost"),
-    (prv.load('tests_nonghost_calibration.conf',
-              statistic_mode="Spatial|Temporal",
-              statistic_aggregation="Median",
-              tests=True),
+    (prv.Providentia('tests_nonghost_calibration.conf',
+                     statistic_mode="Spatial|Temporal",
+                     statistic_aggregation="Median",
+                     tests=True),
      "spatial_temporal", "nonghost"),
-    (prv.load('tests_nonghost_calibration.conf',
-              tests=True),
+    (prv.Providentia('tests_nonghost_calibration.conf',
+                     tests=True),
      "temporal_spatial", "nonghost")
 ]
 
@@ -49,10 +49,12 @@ possibilities = [
 @pytest.mark.parametrize("inst, statistic_mode, network_type", possibilities[0:6])
 def test_read_data(inst, statistic_mode, network_type):
     path = f'tests/reference/{network_type}/{statistic_mode}/data/data.npy'
+    inst.load()
     read_data(inst, path)
 
 
 @pytest.mark.parametrize("inst, statistic_mode, network_type", possibilities[6:9])
 def test_calibration(inst, statistic_mode, network_type):
     path = f'tests/reference/{network_type}/{statistic_mode}/data/data_calibration.npy'
+    inst.load()
     read_data(inst, path)
