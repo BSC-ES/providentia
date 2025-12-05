c0010a3bda9a208eac6f711c4c8211d29ff04bad
Alba Vilanova <alba.vilanova@outlook.com>
2025-12-05 09:06:04 +0100

    Remove unused tests

--- tests/test_save.py
+++ tests/test_save.py
@@ -29,36 +29,23 @@ possibilities = [
      "spatial_temporal", "nonghost"),
     (prv.Providentia('tests_nonghost.conf',
                      tests=True),
-     "temporal_spatial", "nonghost"),
-    (prv.Providentia('tests_nonghost_calibration.conf',
-                     statistic_mode="Flattened",
-                     statistic_aggregation="",
-                     tests=True),
-     "flattened", "nonghost"),
-    (prv.Providentia('tests_nonghost_calibration.conf',
-                     statistic_mode="Spatial|Temporal",
-                     statistic_aggregation="Median",
-                     tests=True),
-     "spatial_temporal", "nonghost"),
-    (prv.Providentia('tests_nonghost_calibration.conf',
-                     tests=True),
      "temporal_spatial", "nonghost")
 ]
 
 
-@pytest.mark.parametrize("inst, statistic_mode, network_type", possibilities[0:6])
+@pytest.mark.parametrize("inst, statistic_mode, network_type", possibilities)
 def test_save_conf(inst, statistic_mode, network_type):
     inst.load()
     save_data(inst, 'conf', 'data', network_type, statistic_mode)
 
 
-@pytest.mark.parametrize("inst, statistic_mode, network_type", possibilities[0:6])
+@pytest.mark.parametrize("inst, statistic_mode, network_type", possibilities)
 def test_save_npz(inst, statistic_mode, network_type):
     inst.load()
     save_data(inst, 'npz', 'data', network_type, statistic_mode)
 
 
-@pytest.mark.parametrize("inst, statistic_mode, network_type", possibilities[0:6])
+@pytest.mark.parametrize("inst, statistic_mode, network_type", possibilities)
 def test_save_netcdf(inst, statistic_mode, network_type):
     inst.load()
     save_data(inst, 'nc', 'data', network_type, statistic_mode)
