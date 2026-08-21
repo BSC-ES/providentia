91aaa1c397ff46e1e384de28903afd68a41b8f7c
Alba Vilanova <alba.vilanova@bsc.es>
2026-08-21 12:18:04 +0200

    Revert change

--- tests/aux_functions.py
+++ tests/aux_functions.py
@@ -79,7 +79,10 @@ def check_filter_data(inst, statistic_mode, network_type, filter):
     # Check filtered data is different from original data
     orig_output = np.load(orig_path, allow_pickle=True)
     filter_output = np.load(filter_path, allow_pickle=True)
-    assert not np.allclose(orig_output, filter_output, equal_nan=True)
+    try:
+        assert not np.allclose(orig_output, filter_output, equal_nan=True)
+    except ValueError:
+        assert True
 
 
 def save_data(inst, format, fname, network_type, statistic_mode):
